bf7ac732239959401d2fdf2d02c8819ca3890a7d
Frank <30297768+ilknarf@users.noreply.github.com>
2024-08-21 00:15:00 -0500

    add examples, update README.md

--- README.md
+++ README.md
@@ -1,3 +1,56 @@
-# Iterbones
+# PIter
 
-### Iterator helpers in go
+## Parallel Consumers using Go Iterators
+
+- Required Go version: `1.23+`
+
+### Explanation
+As seen [here](https://go.dev/blog/range-functions#passing-a-function-to-a-push-iterator), Go's new push iterators can take in arbitrary `yield` functions. Taking advantage of this, we can easily create iterator consumers that
+:
+1. Have simple Go error handling semantics.
+2. Can process jobs in parallel using a set number of workers.
+3. Utilize existing idiomatic and time-tested patterns to do so (`errgroup.Group`).
+
+### Use case
+- This library's goal is to provide a simple surface to enable parallel iterator consumption. Potential use cases:
+    - Batch processing
+	- Consuming streams in a worker pool
+	- Lambda handlers (Is this even optimal?)
+- Perfomance implications aside, this library can also enable incremental refactoring of code through simplification of iterative code into declarative processing.
+
+### Caveats
+- `slices.PFlatMap` and`slices.PMap` DO NOT preserve order.
+- `maps.PMap` overwrites duplicate keys in an undeterministic fashion. This is irrelevant when processing maps or slices, but more important when processing `iter.Seq2` with different semantics.
+
+### Usage
+```go
+package main
+
+import (
+	"slices"
+
+	pslices "github.com/ilknarf/piter/slices"
+)
+
+func taskHandler(ctx context.Context, ids []string) (*Result, error) {
+	// do important stuff
+	return res, nil
+}
+
+func main() {
+	ctx := context.Background()
+	ids := []string{"id1", "id2", "id3", "id4", "id5"} // ...
+	batchIter := slices.Chunk(ids)
+
+	// number of threads. this is directly passed to `errgroup.Group.SetLimit`
+	batchSize := 10
+
+	// in parallel
+	res, err := pslices.PMap(ctx, batchIter, batchSize, taskHandler)
+
+	// ...
+}
+
+```
+
+See [examples](examples/) for more... examples.
---
+++ examples/ombd/main.go
@@ -0,0 +1,155 @@
+package main
+
+import (
+	"context"
+	"encoding/json"
+	"fmt"
+	"io"
+	"log"
+	"net/http"
+	"net/url"
+	"os"
+	"slices"
+	"time"
+
+	pslices "github.com/ilknarf/piter/slices"
+)
+
+const threadLimit = 10
+
+type Result struct {
+	Response string
+	Search   []*Entry `json:"Search"`
+}
+
+type Entry struct {
+	Title string `json:"Title"`
+	Type  string `json:"Type"`
+	Year  string `json:"Year"`
+}
+
+// a list of kw to query for
+var keyword = []string{
+	"Friends",
+	"Deadpool",
+	"X-Men",
+	"Arcane",
+	"Citizen Kane",
+	"Seinfeld",
+	"I Love Lucy",
+	"Severence",
+	"Rings of Power",
+	"Start Trek",
+	"Foundation",
+	"The Boys",
+	"Fleabag",
+	/*
+		"No more matches",
+		"No more matches",
+		"No more matches",
+		"No more matches",
+		"No more matches",
+		"No more matches",
+		"No more matches",
+		"No more matches",
+		"No more matches",
+		"No more matches",
+		"No more matches",
+		"No more matches",
+		"No more matches",
+		"No more matches",
+		"No more matches",
+		"No more matches",
+		"No more matches",
+		"No more matches",
+		"No more matches",
+		"No more matches",
+		"No more matches",
+		"No more matches",
+		"No more matches",
+		"No more matches",
+		"No more matches",
+		"No more matches",
+		"No more matches",
+		"No more matches",
+		"No more matches",
+		"No more matches",
+		"No more matches",
+		"No more matches",
+		"No more matches",
+		"No more matches",
+		"No more matches",
+		"No more matches",
+	*/
+}
+
+var ombdKey = os.Getenv("OMDB_KEY")
+
+func sendOmdbRequest(ctx context.Context, url string) (*Result, error) {
+	req, err := http.NewRequest("GET", url, nil)
+	if err != nil {
+		return nil, err
+	}
+	req = req.WithContext(ctx)
+
+	resp, err := http.DefaultClient.Do(req)
+	if err != nil {
+		return nil, err
+	}
+
+	defer resp.Body.Close()
+
+	// JSON stuff
+	body, err := io.ReadAll(resp.Body)
+	if err != nil {
+		return nil, err
+	}
+
+	result := &Result{}
+
+	err = json.Unmarshal(body, result)
+	if err != nil {
+		return nil, err
+	}
+
+	return result, nil
+}
+
+func searchOmdb(ctx context.Context, kw string) ([]*Entry, error) {
+	// HTTP request stuff
+	url := fmt.Sprintf("https://www.omdbapi.com/?s=%s&apiKey=%s", url.QueryEscape(kw), ombdKey)
+
+	result, err := sendOmdbRequest(ctx, url)
+	if err != nil {
+		return nil, err
+	}
+
+	// return []*Entry
+	return result.Search, nil
+}
+
+func main() {
+	// something like 1000 request/day on the free key
+
+	// 30 second timeout for the script
+	ctx, cancel := context.WithTimeout(context.Background(), 30*time.Second)
+	defer cancel()
+
+	log.Println("getting results")
+	res, err := pslices.PFlatMap(ctx, slices.Values(keyword), threadLimit, searchOmdb)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	log.Println("printing results")
+
+	// print results
+	for _, entry := range res {
+		b, err := json.Marshal(entry)
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		fmt.Println(string(b))
+	}
+}
